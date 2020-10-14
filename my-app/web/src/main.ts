531db567497574d686db0d5575b2d82d60a1a0ba
Fuyf <fuyunfeng@idcmatic.com>
2020-10-14 16:37:03 +0800

    vconsole

--- my-app/web/src/main.ts
+++ my-app/web/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { Popup, Cell, CellGroup, Switch } from 'vant';
+import Vconsole from 'vconsole'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 const app = createApp(App);
+const vConsole: any = new Vconsole()
 
 // vant
 app.use(Popup)
@@ -12,7 +15,10 @@ app.use(Cell)
 app.use(CellGroup)
 app.use(Switch)
 
+app.use(vConsole)
 app.use(store)
 app.use(router)
 
 app.mount('#app')
+
+export default app
