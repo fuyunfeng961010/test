3fff736c8b5a14e92bac9a89d433dbc4cb340cc8
fuyf <fuyunfeng@gds-services.com>
2021-02-18 16:53:16 +0800

    InAppBrowser

--- hello-cordova/web/src/cordova/AppBrowser.js
+++ hello-cordova/web/src/cordova/AppBrowser.js
@@ -1,7 +1,8 @@
 /**
  * cordova-plugin-inappbrowser
- * 白名单内的域名 用cordova webview打开 会导致后台唤醒时退出打开的webhview
- * window.location.href 则不会  需白名单内配置
+ * 未配置白名单时 window.location.href会使用外部浏览器打开页面  当前插件正常调用
+ * 白名单内的域名 当前插件调用也会使用自带的webView打开而非插件webView 但会在后台唤醒时退出打开的webhview 回首页
+ * window.location.href 则不会
  */
 const init = () => {
   console.log('cordova.InAppBrowser', cordova.InAppBrowser)
--- hello-cordova/web/src/views/plugin-page/plugins/InAppBrowser.vue
+++ hello-cordova/web/src/views/plugin-page/plugins/InAppBrowser.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="in-app-browser-container">
-    in-app-browser
+    <van-button block type="primary" @click="openWebview(false)">默认</van-button>
+    <van-button block type="primary" @click="openWebview(true)" class="offset-px-mt-10">InAppBrowser</van-button>
   </div>
 </template>
 
 <script>
-import { cell } from 'vant'
+import { getCurrentInstance } from 'vue'
+import { cell, Button } from 'vant'
 const app = {
   name: 'InAppBrowser',
   components: {
-    [cell.name]: cell
+    [cell.name]: cell,
+    [Button.name]: Button
   },
   setup() {
+    const { $AppBrowser } = getCurrentInstance().appContext.config.globalProperties
+
+    const openWebview = (isInAppBrowser = false) => {
+      console.log('isInAppBrowser', isInAppBrowser)
+      const url = 'https://cordova-mobile.fuyunfeng.top/'
+      if (isInAppBrowser) {
+        return $AppBrowser.open(url, '_self', 'location=yes')
+      }
+      window.location.href = url
+    }
     return {
+      openWebview
     }
   }
 
