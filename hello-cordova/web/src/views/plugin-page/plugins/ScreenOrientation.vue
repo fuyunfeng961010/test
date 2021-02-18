0d5cb17b33113da33270d14f59a1d6d7f97e1c2d
fuyf <fuyunfeng@gds-services.com>
2021-02-18 17:19:57 +0800

    ScreenOrientation

--- hello-cordova/web/src/views/plugin-page/plugins/ScreenOrientation.vue
+++ hello-cordova/web/src/views/plugin-page/plugins/ScreenOrientation.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="screen-orientation-container">
-    screen-orientation
+    <van-button block type="primary" @click="setScreen('landscape-primary')"
+      >横向</van-button
+    >
+    <van-button
+      block
+      type="primary"
+      @click="setScreen('portrait-primary')"
+      class="offset-px-mt-10"
+      >纵向</van-button
+    >
+    <van-button block type="primary" @click="unScreen" class="offset-px-mt-10"
+      >解锁</van-button
+    >
+    <van-button block type="primary" @click="getScreen" class="offset-px-mt-10"
+      >当前</van-button
+    >
   </div>
 </template>
 
 <script>
-import { cell } from 'vant'
+import { getCurrentInstance } from 'vue'
+import { cell, Button, Toast } from 'vant'
 const app = {
   name: 'ScreenOrientation',
   components: {
-    [cell.name]: cell
+    [cell.name]: cell,
+    [Button.name]: Button,
+    [Toast.name]: Toast
   },
   setup() {
+    const { $ScreenOrientation } = getCurrentInstance().appContext.config.globalProperties
+    const setScreen = (type) => {
+      $ScreenOrientation.lock(type)
+    }
+
+    const unScreen = () => {
+      $ScreenOrientation.unlock()
+    }
+
+    const getScreen = () => {
+      Toast({
+        message: $ScreenOrientation.getOrientation(),
+        position: 'bottom'
+      })
+    }
     return {
+      setScreen,
+      unScreen,
+      getScreen
     }
   }
 
