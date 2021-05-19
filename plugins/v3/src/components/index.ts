2ed04a249b8579db5d23d676183b0589ee7607a9
fuyf <fuyunfeng@gds-services.com>
2021-05-19 17:08:29 +0800

    v3  SliderVerify

--- plugins/v3/src/components/index.ts
+++ plugins/v3/src/components/index.ts
@@ -1,12 +1,13 @@
 import { App } from 'vue'
 
-import toast from './Toast/index';
-import Button from './Button/index.vue';
+// import toast from './Toast/index';
+// import Button from './Button/index.vue';
+import SliderVerify from './SliderVerify/index.vue';
 
 const install = function (app: App): void {
-  app.provide('$toast', toast)
-
-  app.component(Button.name, Button)
+  // app.provide('$toast', toast)
+  // app.component(Button.name, Button)
+  app.component(SliderVerify.name, SliderVerify)
 }
 
 // 默认导出 install
